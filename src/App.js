4a0505914802df47af51113df948003ca053d287
unknown <otz4193@naver.com>
2023-03-22 16:54:13 +0900

    test6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React/* , { useState, useEffect }  */from 'react';
-import { /* BrowserRouter, */HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter,/* HashRouter, */ Routes, Route } from 'react-router-dom';
 
 import Bios from './components/Bios/Bios';
 import Loading from './components/Loading/Loading';
@@ -13,7 +13,7 @@ function App() {
   const store = createStore(reducer);
 
   return (
-        <HashRouter>
+        <BrowserRouter>
         <Provider store={store} >
             <Routes >
                 <Route path="/" element={<Bios />}></Route>
@@ -21,7 +21,7 @@ function App() {
                 <Route path="/windows" element={<Windows />}></Route>
             </Routes>
           </Provider>
-        </HashRouter> 
+        </BrowserRouter> 
   );
 }
 //"homepage": "https://nuyheshub.github.io/pilot-test-win/",
