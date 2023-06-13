a9ff93d0d78f4a5c4bcd44eeb40b0befb6b4467a
joosehyeon <7upbeat7@naver.com>
2023-06-14 00:18:43 +0900

    analytics test

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID); // 여기에 Google 애널리틱스 추적 ID를 입력하세요
+  ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID); // 여기에 Google 애널리틱스 추적 ID를 입력
 };
 
 export const logPageView = () => {
