22c300fc70ef548fef0d130e39f1b8d77a515c1b
joosehyeon <7upbeat7@naver.com>
2023-06-14 00:12:46 +0900

    analytics add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React/* , { useState, useEffect }  */from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
-/* import {  HashRouter, Routes, Route } from 'react-router-dom'; */
 
 import Bios from './components/Bios/Bios';
 import Loading from './components/Loading/Loading';
@@ -9,15 +8,20 @@ import './styles/total_style.scss'
 import {Provider} from 'react-redux'
 import { createStore } from 'redux';
 import reducer from './store/reducers/reducer'
+import { initGA, logPageView } from './analytics';
 
 function App() {
   const store = createStore(reducer);
+  
+  useEffect(() => {
+    initGA(); // Google 애널리틱스 초기화
+    logPageView(); // 페이지 뷰 추적
+  }, []);
 
   return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store} >
             <Routes >
-                {/* <Route path={"/pilot-test-win"} element={<Bios />}></Route> */}
                 <Route path="/"  element={<Bios/>}></Route>
                 <Route path="/loading" element={<Loading/>}></Route>
                 <Route path="/Windows" element={<Windows/>}></Route>
@@ -28,8 +32,10 @@ function App() {
   );
 }
 
+export default App;
+
+/* <Route path={"/pilot-test-win"} element={<Bios />}></Route> */
 //"homepage": "https://nuyheshub.github.io/pilot-test-win/",
 //<Route path={'/pilot-test-win'}element={<Bios />}></Route>
 //{`${process.env.PUBLIC_URL}/`} 
 ///*  basename={process.env.PUBLIC_URL} */
-export default App;
